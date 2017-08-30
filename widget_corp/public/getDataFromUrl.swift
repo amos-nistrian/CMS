08b978cb6cb794c622ef55de57420527706cf577
Amos Nistrian <amos-nistrian@users.noreply.github.com>
2017-08-29 21:21:52 -0700

    Delete getDataFromUrl.swift

--- widget_corp/public/getDataFromUrl.swift
+++
@@ -1,6 +0,0 @@
-func getDataFromUrl(url: URL, completion: @escaping (_ data: Data?, _  response: URLResponse?, _ error: Error?) -> Void) {
-    URLSession.shared.dataTask(with: url) {
-        (data, response, error) in
-        completion(data, response, error)
-        }.resume()
-}
